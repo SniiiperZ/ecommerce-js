39780627b96eb1ea832a7126d4c695184ecf963f
SniiiperZ <cedric1997@hotmail.com>
2024-06-26 16:11:52 +0200

    petite modif

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -5,6 +5,7 @@ import {
   clearCart,
 } from "../storage/Cart";
 
+// Fonction pour mettre à jour le compteur d'articles dans le panier
 const updateCartCount = () => {
   const cart = JSON.parse(localStorage.getItem("cart")) || [];
   const cartCount = cart.reduce(
@@ -14,9 +15,11 @@ const updateCartCount = () => {
   document.getElementById("cart-count").textContent = cartCount;
 };
 
+// Fonction pour afficher le contenu du panier
 export const Cart = (element) => {
   const cart = getCart();
 
+  // Fonction pour calculer le prix total du panier
   const calculateTotalPrice = () => {
     return cart.reduce(
       (total, produit) =>
@@ -74,35 +77,39 @@ export const Cart = (element) => {
     </div>
   `;
 
+  // Ajout des écouteurs d'événements pour les boutons de mise à jour et de suppression
   document.querySelectorAll(".quantity-input").forEach((input) => {
     input.addEventListener("change", (event) => {
       const productId = parseInt(event.target.getAttribute("data-id"));
       const quantity = parseInt(event.target.value);
-      updateProductQuantity(productId, quantity);
+      updateProductQuantity(productId, quantity); // Mettre à jour la quantité du produit dans le panier
       updateCartCount(); // Mettre à jour le compteur de panier
       Cart(element); // Re-render the cart
     });
   });
 
+  // Ajout d'un écouteur d'événement pour le bouton de suppression
   document.querySelectorAll(".remove-btn").forEach((button) => {
     button.addEventListener("click", (event) => {
       const productId = parseInt(event.target.getAttribute("data-id"));
-      removeProductFromCart(productId);
+      removeProductFromCart(productId); // Supprimer le produit du panier
       updateCartCount(); // Mettre à jour le compteur de panier
       Cart(element); // Re-render the cart
     });
   });
 
+  // Ajout d'un écouteur d'événement pour le bouton "Vider le panier"
   document.getElementById("clear-cart-btn").addEventListener("click", () => {
-    clearCart();
+    clearCart(); // Vide le panier
     updateCartCount(); // Mettre à jour le compteur de panier
     Cart(element); // Re-render the cart
   });
 
+  // Ajout d'un écouteur d'événement pour le bouton "Commander"
   document.getElementById("checkout-btn").addEventListener("click", () => {
     alert("Commande simulée avec succès !");
-    clearCart();
+    clearCart(); // Vide le panier après la commande
     updateCartCount(); // Mettre à jour le compteur de panier
-    Cart(element); // Re-render the cart
+    Cart(element); //
   });
 };
